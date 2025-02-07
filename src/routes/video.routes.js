b69fab141bb0ec6b7ea7b50c5965b5707111dbb9
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-02-07 13:13:20 +0530

    feat: add video retrieval and management routes including fetching, updating, and deleting videos

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -2,25 +2,40 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { upload } from '../middlewares/multer.middlewares.js'
 import { 
-      publishVideo 
+      deleteVideo,
+      getAllVideos,
+      getVideoById,
+      publishVideo, 
+      togglePublishStatus, 
+      updateVideo
 } from "../controllers/video.controllers.js";
 
 const router = Router()
 
 router.use(verifyJWT)
 
-router.route('/').post(
-      upload.fields([
-            {
-                  name: 'video',
-                  maxCount: 1
-            },
-            {
-                  name: 'thumbnail',
-                  maxCount: 1
-            }
-      ]),
-      publishVideo
-)
+router.route('/')
+            .post(
+                  upload.fields([
+                        {
+                              name: 'video',
+                              maxCount: 1
+                        },
+                        {
+                              name: 'thumbnail',
+                              maxCount: 1
+                        }
+                  ]),
+                  publishVideo
+            )
+            .get(getAllVideos)
+
+router.route('/:videoId')
+            .get(getVideoById)
+            .patch(upload.single("thumbnail"), updateVideo)
+            .delete(deleteVideo)
+
+router.route('/toggle/publish/:videoId')
+            .patch(togglePublishStatus)
 
 export default router
